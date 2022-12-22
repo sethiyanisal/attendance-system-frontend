14b1d0e2777b3cb3879b2c1f9dc1ea1ea2703932
sethiyanisal <sethiya.peellapitiya@kodez.com.au>
2022-12-22 13:22:50 +0530

    added time and date states

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import SignUp from './components/SignUp';
 import UserDashboard from './components/user/UserDashboard';
 import UserLeaveRequestList from './components/user/UserLeaveRequestList';
 import UserLeaveRequestForm from './components/user/UserLeaveRequestForm';
-// import TimeCard from './components/user/TimeCard';
-// import PunchTime from './components/user/PunchTime';
+import TimeCard from './components/user/TimeCard';
+import PunchTime from './components/user/PunchTime';
+// import TimeClock from './components/user/TimeClock';
 
 
 
@@ -24,10 +25,11 @@ function App() {
           <Route path="/" element={<Login />}></Route>
           <Route path="/register" element={<SignUp />}></Route>
           <Route path="/user/dashboard" element={<UserDashboard />}></Route>
-          {/* <Route path="/user/punchtime" element={<PunchTime/>}></Route>
-          <Route path="/user/timecards" element={<TimeCard />}></Route> */}
+          <Route path="/user/punchtime" element={<PunchTime/>}></Route>
+          <Route path="/user/timecards" element={<TimeCard />}></Route>
           <Route path="/user/leaverequests" element={<UserLeaveRequestList/>}></Route>
           <Route path="/user/leaverequests/leaverequestform" element={<UserLeaveRequestForm/>}></Route>
+          {/* <Route path="/user/timeclock" element={<TimeClock/>}></Route> */}
         </Routes>
       </Router>
     </div>
