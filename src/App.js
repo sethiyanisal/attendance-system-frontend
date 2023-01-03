ef3700de43399ea1e6b73d940fcd1a75132dcd79
sethiyanisal <sethiya.peellapitiya@kodez.com.au>
2023-01-03 17:07:42 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import UserLeaveRequestForm from './components/user/UserLeaveRequestForm';
 import TimeCard from './components/user/TimeCard';
 import PunchTime from './components/user/PunchTime';
 import Layout from './components/Layout';
-import RequireAuth from './components/RequireAuth';
 
 const ROLES = {
   'User': 2080,
@@ -29,15 +28,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/register" element={<SignUp />}></Route>
-            <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-              <Route path="/user/dashboard" element={<UserDashboard />}></Route>
-              <Route path="/user/punchtime" element={<PunchTime/>}></Route>
-              <Route path="/user/timecards" element={<TimeCard />}></Route>
-              <Route path="/user/leaverequests" element={<UserLeaveRequestList/>}></Route>
-              <Route path="/user/leaverequests/leaverequestform" element={<UserLeaveRequestForm/>}></Route>
-            </Route>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/register" element={<SignUp />}></Route>
+          <Route path="/user/dashboard" element={<UserDashboard />}></Route>
+          <Route path="/user/punchtime" element={<PunchTime/>}></Route>
+          <Route path="/user/timecards" element={<TimeCard />}></Route>
+          <Route path="/user/leaverequests" element={<UserLeaveRequestList/>}></Route>
+          <Route path="/user/leaverequests/leaverequestform" element={<UserLeaveRequestForm/>}></Route>
           </Route>
         </Routes>
       </Router>
