57e738c29e353e208706df3db0eaa40429e6b494
sethiyanisal <sethiya.peellapitiya@kodez.com.au>
2023-01-05 13:04:01 +0530

    modified requireAuth

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import UserLeaveRequestForm from './components/user/UserLeaveRequestForm';
 import TimeCard from './components/user/TimeCard';
 import PunchTime from './components/user/PunchTime';
 import Layout from './components/Layout';
+import RequireAuth from './components/RequireAuth';
+import Missing from './components/Missing';
 
 const ROLES = {
   'User': 2080,
@@ -25,19 +27,25 @@ const ROLES = {
 function App() {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/register" element={<SignUp />}></Route>
-          <Route path="/user/dashboard" element={<UserDashboard />}></Route>
-          <Route path="/user/punchtime" element={<PunchTime/>}></Route>
-          <Route path="/user/timecards" element={<TimeCard />}></Route>
-          <Route path="/user/leaverequests" element={<UserLeaveRequestList/>}></Route>
-          <Route path="/user/leaverequests/leaverequestform" element={<UserLeaveRequestForm/>}></Route>
+            
+            {/* public routes */}
+            <Route path="/" element={<Login />}></Route>
+            <Route path="/register" element={<SignUp />}></Route>
+
+            <Route element={<RequireAuth/>}>
+              <Route path="/user/dashboard" element={<UserDashboard />}></Route>
+              <Route path="/user/punchtime" element={<PunchTime/>}></Route>
+              <Route path="/user/timecards" element={<TimeCard />}></Route>
+              <Route path="/user/leaverequests" element={<UserLeaveRequestList/>}></Route>
+              <Route path="/user/leaverequests/leaverequestform" element={<UserLeaveRequestForm/>}></Route>
+            </Route>
+
+            <Route path="/*" element={<Missing />} />
+
           </Route>
         </Routes>
-      </Router>
     </div>
   );
 }
--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,16 +1,14 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 
-const RequireAuth = ({ allowedRoles }) => {
-    const { auth } = useAuthContext();
+const RequireAuth = () => {
+    const { context } = useAuthContext();
     const location = useLocation();
 
     return (
-        auth?.roles?.find(role => allowedRoles?.includes(role))
+        context?.user
             ? <Outlet />
-            : auth?.user
-                ? <Navigate to="/" state={{ from: location }} replace />
-                : <Navigate to="/" state={{ from: location }} replace />
+            : <Navigate to="/" state={{ from: location }} replace />
     );
 }
 
