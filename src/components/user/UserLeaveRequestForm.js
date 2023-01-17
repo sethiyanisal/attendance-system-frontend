424f64f258cfec05e321a678621dc7e762cbf93a
sethiyanisal <sethiya.peellapitiya@kodez.com.au>
2023-01-17 14:26:56 +0530

    added the useeffect function to leaveform

--- src/components/user/UserLeaveRequestForm.js
+++ src/components/user/UserLeaveRequestForm.js
@@ -6,18 +6,52 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Image from './../../images/logo.png';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import leaveRequestService from '../../routes/leaveRequestServiceRoutes';
 
 
 const UserLeaveRequestForm = () => {
+
+const { auth } = useAuthContext();
+const [userData, setUser] = useState();
+const [userFirstname, setFirstName] = useState();
+const [userLasttname, setLastName] = useState();
+const [userEmail, setEmail] = useState();
+const [userContactNum, setContactNum] = useState();
+const [userLeaveType, setLeaveType] = useState();
+
+  const handleChange = (e) => {
+    setLeaveType(e.target.value);
+  };
+
+useEffect(() => {
+    const userID = auth.user.id;
+    const token = auth.user.token;
+      leaveRequestService
+        .getUserDetailsById(userID, token)
+        .then((res) => {
+          setUser(res.data.data);
+          setFirstName(res.data.data.firstName);
+          setLastName(res.data.data.lastName);
+          setEmail(res.data.data.email);
+          setContactNum(res.data.data.contactnum);
+          console.log()
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }, []);
     
-  return (
+return (
     <>
-        <Box sx={{
-                    width:1,
-                    flexDirection: 'column',
-                    paddingTop:2,
-                }}>
+    {[userData]?.map((item, index) => {
+        return(
+            <Box key={index} sx={{
+                width:1,
+                flexDirection: 'column',
+                paddingTop:2,
+            }}>
                 <Box
                     sx={{
                         width: 120,
@@ -40,125 +74,129 @@ const UserLeaveRequestForm = () => {
                     <ArrowBackIosNewRounded/>
                 </Box>
                 <Box sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems:'flex-start',
-                  marginLeft:30
-                  }}
-                  >
-                  <Typography component="h1" variant="h5" sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems:'flex-start',
+                marginLeft:30
+                }}
+                >
+                <Typography component="h1" variant="h5" sx={{
                     paddingTop:3,
                     paddingBottom:2,
                     }}>
-                      Leave Requests
-                  </Typography>
+                    Leave Requests
+                </Typography>
                 </Box>
 
                 <Box component="form" noValidate sx={{ mt: 3, ml:30, width: 1000, }}>
-                <Grid container spacing={4}>
-                <Grid item xs={12} sm={6}>
+                    <Grid container spacing={4}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        value={item?.firstName || ""}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        value={item?.lastName || ""}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        value={item?.email || ""}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        required
+                        fullWidth
+                        id="contactnum"
+                        label="Contact Number"
+                        name="contactnum"
+                        value={item?.contactnum || ""}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                    <FormControl fullWidth>
+                    <InputLabel id="leave-type">Leave Type</InputLabel>
+                        <Select
+                            labelId="leave-type"
+                            id="leave-type"
+                            label="leave-type"
+                            value={userLeaveType}
+                            onChange={handleChange}
+                        >
+                            <MenuItem >Personal Leave</MenuItem>
+                            <MenuItem >Medical Leave</MenuItem>
+                            <MenuItem >Annual Leave</MenuItem>
+                        </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type="date"
+                            required
+                            fullWidth
+                            id="datefrom"
+                            label="From"
+                            name="datefrom"
+                            defaultValue="2019-05-24"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
                     <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
+                            type="date"
+                            required
+                            fullWidth
+                            id="dateto"
+                            label="To"
+                            name="dateto"
+                            defaultValue="2019-05-24"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
                     <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email"
-                    name="email"
-                    autoComplete="email"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                    required
-                    fullWidth
-                    id="contactnum"
-                    label="Contact Number"
-                    name="contactnum"
-                    autoComplete="contactnum"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                <FormControl fullWidth>
-                <InputLabel id="leave-type">Leave Type</InputLabel>
-                    <Select
-                        labelId="leave-type"
-                        id="leave-type"
-                        label="leave-type"
+                            fullWidth
+                            multiline
+                            label="Subject (reason)"
+                            InputProps={{
+                                inputComponent: TextareaAutosize
+                            }}
+                        />
+                    </Grid>
+                    </Grid>
+                    <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ mt: 4, mb: 4, mr: 0, width:'auto', borderRadius:10,  color: 'white', backgroundColor:'#1D1D1D', borderColor: 'black',
+                    '&:hover': {
+                    backgroundColor: '#393939',
+                    color: 'white',
+                    borderColor:'black'
+                    },   
+                    }}
                     >
-                        <MenuItem >Personal Leave</MenuItem>
-                        <MenuItem >Medical Leave</MenuItem>
-                        <MenuItem >Annual Leave</MenuItem>
-                    </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        type="date"
-                        required
-                        fullWidth
-                        id="datefrom"
-                        label="From"
-                        name="datefrom"
-                        defaultValue="2019-05-24"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                <TextField
-                        type="date"
-                        required
-                        fullWidth
-                        id="dateto"
-                        label="To"
-                        name="dateto"
-                        defaultValue="2019-05-24"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                        fullWidth
-                        multiline
-                        label="Subject (reason)"
-                        InputProps={{
-                            inputComponent: TextareaAutosize
-                        }}
-                    />
-                </Grid>
-                </Grid>
-                <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 4, mb: 4, mr: 0, width:'auto', borderRadius:10,  color: 'white', backgroundColor:'#1D1D1D', borderColor: 'black',
-                '&:hover': {
-                backgroundColor: '#393939',
-                color: 'white',
-                borderColor:'black'
-                },   
-                }}
-                >
-                Send Request
-                </Button>
+                    Send Request
+                    </Button>
+                </Box>
             </Box>
-        </Box>
+        )
+    })}
+        
     </>
   )
 }
--- src/routes/leaveRequestServiceRoutes.js
+++ src/routes/leaveRequestServiceRoutes.js
@@ -8,9 +8,18 @@ const getLeaveRequestsById = (userID, token) => {
   return Axios.get("/user/leaverequestsbyid/" +userID, {headers: headers});
 };
 
+const getUserDetailsById = (userID, token) => {
+  const headers = {
+    // 'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' +token 
+  }
+  return Axios.get("/user/userdetailsbyid/" +userID, {headers: headers});
+};
+
 
 const leaveRequestService = {
-    getLeaveRequestsById
+    getLeaveRequestsById,
+    getUserDetailsById
 };
 
 export default leaveRequestService;
