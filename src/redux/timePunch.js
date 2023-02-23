863fa6d6810b785429ab455318c3d849ce25423d
sethiyanisal <sethiya.peellapitiya@kodez.com.au>
2023-02-23 11:04:59 +0530

    added the redux reducers

--- src/redux/timePunch.js
+++ src/redux/timePunch.js
@@ -3,13 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const timePunchSlice = createSlice({
   name: "timepunch",
   initialState: {
-    count: 0
+    isPunched: false
   },
   reducers: {
+    punchInStatus: (state) => {
+      state.isPunched = true;
+    },
+
+    punchOutStatus: (state) => {
+      state.isPunched = false;
+    }
   }
 });
 
 // Action creators are generated for each case reducer function
-export const {} = timePunchSlice.actions;
+export const {punchInStatus, punchOutStatus} = timePunchSlice.actions;
 
 export default timePunchSlice.reducer;
